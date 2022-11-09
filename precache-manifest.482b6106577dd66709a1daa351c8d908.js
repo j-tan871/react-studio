f80491c4b97324f78bc403936fa997d582686352
j-tan871 <j-tan871@users.noreply.github.com>
2022-11-09 05:27:55 +0000

    deploy: 370bf511266550c434572441b5496a4efbf41b6a

---
+++ precache-manifest.482b6106577dd66709a1daa351c8d908.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "25e89f0a72b33ba57f1d",
+    "url": "/react-studio/static/css/main.0b844a57.chunk.css"
+  },
+  {
+    "revision": "25e89f0a72b33ba57f1d",
+    "url": "/react-studio/static/js/main.25e89f0a.chunk.js"
+  },
+  {
+    "revision": "f6d4ee5088df8ea8b4d3",
+    "url": "/react-studio/static/js/1.f6d4ee50.chunk.js"
+  },
+  {
+    "revision": "8db8c5c4008b46223800",
+    "url": "/react-studio/static/js/runtime~main.8db8c5c4.js"
+  },
+  {
+    "revision": "dc40ace527178d5908adb174a57c819c",
+    "url": "/react-studio/index.html"
+  }
+];
